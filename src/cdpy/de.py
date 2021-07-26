cfb33c4490739045a12a692f49db8239416a2ea2
Alan Silva <alan.silva@cloudera.com>
2021-07-25 21:44:23 -0300

    cdpy changes in de implementation
    Signed-off-by: Alan <alan.silva@cloudera.com>
    (cherry picked from commit f6ac974080d2f89d2f4a515d3366477cdc9cec32)
    Signed-off-by: Curtis Howard <curtis@cloudera.com>
    (cherry picked from commit 054e04a8d9c98273cfe6a0e359c6f197a53dbaef)
    Signed-off-by: curtishoward <curtis@cloudera.com>

--- src/cdpy/de.py
+++ src/cdpy/de.py
@@ -1,96 +1,76 @@
 # -*- coding: utf-8 -*-
 
-from cdpy.common import CdpSdkBase, Squelch, CdpError
+from cdpy.common import CdpError, CdpWarning, CdpSdkBase, Squelch
 
 
 class CdpyDe(CdpSdkBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def list_dbcs(self, cluster_id):
+    def describe_vc(self, cluster_id, vc_id):
         return self.sdk.call(
-            svc='de', func='list_dbcs', ret_field='dbcs', squelch=[
-                Squelch(value='NOT_FOUND', default=list()),
-                Squelch(field='status_code', value='504', default=list(), warning="No dbcs in this Cluster"),
-            ],
-            clusterId=cluster_id
-        )
-
-    def list_vws(self, cluster_id):
-        return self.sdk.call(
-            svc='de', func='list_vws', ret_field='vws', squelch=[
-                Squelch(value='NOT_FOUND', default=list()),
-                Squelch(field='status_code', value='504', default=list(), warning="No vws in this Cluster"),
-            ],
-            clusterId=cluster_id
-        )
-
-    def describe_cluster(self, cluster_id):
-        return self.sdk.call(
-            svc='de', func='describe_cluster', ret_field='cluster', squelch=[
-                Squelch('NOT_FOUND'), Squelch('INVALID_ARGUMENT')
-            ],
-            clusterId=cluster_id
-        )
-
-    def describe_vw(self, cluster_id, vw_id):
-        return self.sdk.call(
-            svc='de', func='describe_vw', ret_field='vw', squelch=[
+            svc='de', func='describe_vc', ret_field='vc', squelch=[
                 Squelch('NOT_FOUND'), Squelch('INVALID_ARGUMENT')
             ],
             clusterId=cluster_id,
-            vwId=vw_id
+            vcId=vc_id
         )
 
-    def describe_dbc(self, cluster_id, dbc_id):
+    def list_vcs(self, cluster_id):
+         return self.sdk.call(
+            svc='de', func='list_vcs', ret_field='vcs', squelch=[
+                Squelch(value='NOT_FOUND', default=list()),
+                Squelch(field='status_code', value='504', default=list(), warning="No VCS in this Cluster"),
+            ],
+            clusterId=cluster_id
+        )
+
+    def create_vc(self, name, cluster_id):
         return self.sdk.call(
-            svc='de', func='describe_dbc', ret_field='dbc', squelch=[
+            svc='de', func='create_vc', ret_field='vc',
+            vcName=name,
+            clusterId=cluster_id
+            # TODO: need to define cpu and mem requests
+        )
+
+    def delete_vc(self, cluster_id, vc_id):
+        return self.sdk.call(
+            svc='de', func='delete_vc', squelch=[Squelch('NOT_FOUND')], clusterId=cluster_id, vcId = vc_id
+        )
+
+    def describe_service(self, cluster_id):
+        return self.sdk.call(
+            svc='de', func='describe_service', ret_field='service', squelch=[
                 Squelch('NOT_FOUND'), Squelch('INVALID_ARGUMENT')
             ],
             clusterId=cluster_id,
-            dbcId=dbc_id
         )
 
-    def list_clusters(self, env_crn=None):
-        resp = self.sdk.call(
-            svc='de', func='list_clusters', ret_field='clusters', squelch=[
+    def list_services(self):
+        return self.sdk.call(
+            svc='de', func='list_services', ret_field='clusters', squelch=[
                 Squelch(value='NOT_FOUND', default=list())
             ]
         )
-        if env_crn:
-            return [x for x in resp if env_crn == x['environmentCrn']]
-        return resp
 
-    def gather_clusters(self, env_crn=None):
-        self.sdk.validate_crn(env_crn)
-        clusters = self.list_clusters(env_crn=env_crn)
-        out = []
-        if clusters:
-            for base in clusters:
-                out.append({
-                    'dbcs': self.list_dbcs(base['id']),
-                    'vws': self.list_vws(base['id']),
-                    **base
-                })
-        return out
-
-    def create_cluster(self, env_crn: str, overlay: bool, aws_public_subnets: list = None,
-                       aws_private_subnets: list = None, az_subnet: str = None, az_enable_az: bool = None):
-        self.sdk.validate_crn(env_crn)
-        if all(x is not None for x in [aws_private_subnets, aws_private_subnets]):
-            aws_options = dict(publicSubnetIds=aws_public_subnets, privateSubnetIds=aws_private_subnets)
-        else:
-            aws_options = None
-        if all(x is not None for x in [az_subnet, az_enable_az]):
-            azure_options = dict(subnetId=az_subnet, enableAZ=az_enable_az)
-        else:
-            azure_options = None
+    def enable_service(self, name, env: str = None, instance_type: str = None, min_instances: str = None, max_instances: str = None):
         return self.sdk.call(
-            svc='de', func='create_cluster', ret_field='clusterId', environmentCrn=env_crn,
-            useOverlayNetwork=overlay, awsOptions=aws_options, azureOptions=azure_options
+            svc='de', func='enable_service', ret_field='service', squelch=[
+                Squelch(value='NOT_FOUND', default=list())
+            ],
+            name=name,
+            env=env,
+            instance-type=instance_type,
+            minimum-instances=min_instances,
+            maximum-instances=max_instances
         )
 
-    def delete_cluster(self, cluster_id: str, force: bool = False):
+    def disable_service(self, cluster_id):
         return self.sdk.call(
-            svc='de', func='delete_cluster', squelch=[Squelch('NOT_FOUND')], clusterId=cluster_id, force=force
+            svc='de', func='disable_service', ret_field='status', squelch=[Squelch('NOT_FOUND')], cluster-id=cluster_id
+        )
+
+    def get_kubeconfig(self, cluster_id):
+        return self.sdk.call(
+            svc='de', func='get_kubeconfig', ret_field='kubeconfig', squelch=[Squelch('NOT_FOUND')], cluster-id=cluster_id
         )
