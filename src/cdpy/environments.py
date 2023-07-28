0fb88fad1f5a2ec4616d309e435602ac8be257d8
Jim Enright <jenright@cloudera.com>
2023-07-28 13:10:41 +0100

    Change proxy name input parameter for idempotency
    Signed-off-by: Jim Enright <jenright@cloudera.com>

--- src/cdpy/environments.py
+++ src/cdpy/environments.py
@@ -15,12 +15,12 @@ class CdpyEnvironments(CdpSdkBase):
         resp = self.list_proxy_configs(name)
         return self.sdk.first_item_if_exists(resp)
 
-    def create_proxy_config(self, name, host=None, port=None, protocol=None, description=None,
+    def create_proxy_config(self, proxyConfigName, host=None, port=None, protocol=None, description=None,
                             noProxyHosts=None, user=None, password=None):
 
         return self.sdk.call(
             svc='environments', func='create_proxy_config', ret_field='proxyConfig',
-            proxyConfigName=name, host=host, port=port,
+            proxyConfigName=proxyConfigName, host=host, port=port,
             protocol=protocol, description=description, noProxyHosts=noProxyHosts, user=user, password=password
         )
 
